ca03ce21afcde5cb2bcaa425fd97c5ad920461d2
Colin <colin.bernet@cern.ch>
2015-06-11 15:03:30 +0200

    merging of 3 first zero bias samples

--- Colin/MetScanning/skimming/merge_cfg.py
+++ Colin/MetScanning/skimming/merge_cfg.py
@@ -1,16 +1,21 @@
 import FWCore.ParameterSet.Config as cms
 
+from PhysicsTools.HeppyCore.utils.dataset import EOSDataset
+
 ##____________________________________________________________________________||
 process = cms.Process("MERGE")
 
 ##____________________________________________________________________________||
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
+zerobias1 = EOSDataset('/store/group/phys_jetmet/schoef/private0TSkim_v0/ZeroBias1/crab_ZeroBias1_Run2015A-PromptReco-v1_RECO/150608_144900/0000', '.*root')
+zerobias2 = EOSDataset('/store/group/phys_jetmet/schoef/private0TSkim_v0/ZeroBias2/crab_ZeroBias2_Run2015A-PromptReco-v1_RECO/150608_144908/0000', '.*root')
+zerobias3 = EOSDataset('/store/group/phys_jetmet/schoef/private0TSkim_v0/ZeroBias3/crab_ZeroBias3_Run2015A-PromptReco-v1_RECO/150608_145006/0000', '.*root')
 ##____________________________________________________________________________||
 process.source = cms.Source(
     "PoolSource",
     fileNames = cms.untracked.vstring(
-        "/store/group/phys_jetmet/schoef/private0TSkim_v0/ZeroBias1/crab_ZeroBias1_Run2015A-PromptReco-v1_RECO/150608_144900/0000/skim_99.root"
+        zerobias3.listOfGoodFiles()
         )
     )
 
