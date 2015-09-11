3c388b1411a5b33a47bcb1e74b6e7992e0501e69
Colin <colin.bernet@cern.ch>
2015-09-11 15:42:11 +0200

    miniaod met analysis and tree

---
+++ Colin/MetScanning/analysis/miniaod_cfg.py
@@ -0,0 +1,54 @@
+import os
+import PhysicsTools.HeppyCore.framework.config as cfg
+
+# input component 
+# several input components can be declared,
+# and added to the list of selected components
+
+#inputSample = cfg.Component(
+#    'test_component',
+#    files = ['doublemu_miniaod.root'],
+#    )
+
+from Colin.MetScanning.samples.miniaod import doublemu
+
+selectedComponents  = [doublemu]
+
+comp = selectedComponents[0]
+comp.files = comp.files[:200]
+comp.splitFactor = 10
+
+from Colin.MetScanning.analyzers.MiniAODReader import MiniAODReader
+source = cfg.Analyzer(
+    MiniAODReader,
+    )
+
+from Colin.MetScanning.analyzers.MetAnalyzer import MetAnalyzer
+met_ana = cfg.Analyzer(
+    MetAnalyzer,
+    )
+
+from Colin.MetScanning.analyzers.MetTreeProducer import MetTreeProducer
+met_tree = cfg.Analyzer(
+    MetTreeProducer,
+    tree_name = 'events',
+    tree_title = 'MET tree'
+    )
+
+
+# definition of a sequence of analyzers,
+# the analyzers will process each event in this order
+sequence = cfg.Sequence( [
+        source,
+        met_ana,
+        met_tree
+    ] )
+
+# finalization of the configuration object. 
+from PhysicsTools.HeppyCore.framework.eventsfwlite import Events
+config = cfg.Config( components = selectedComponents,
+                     sequence = sequence, 
+                     services = [],
+                     events_class = Events)
+
+print config
