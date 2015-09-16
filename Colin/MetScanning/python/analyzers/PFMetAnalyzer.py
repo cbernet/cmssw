d98889ed827f4c439a7da6597f8d3015e5980306
Colin <colin.bernet@cern.ch>
2015-09-16 13:08:45 +0200

    processing low met events. new reco cfg

--- Colin/MetScanning/python/analyzers/PFMetAnalyzer.py
+++ Colin/MetScanning/python/analyzers/PFMetAnalyzer.py
@@ -3,7 +3,7 @@
 from PhysicsTools.Heppy.analyzers.core.Analyzer import Analyzer
 from PhysicsTools.Heppy.analyzers.core.AutoHandle import AutoHandle
 from PhysicsTools.Heppy.physicsobjects.PhysicsObject import PhysicsObject as PFCandidate
-from Colin.MetScanning.analyzers.MET import MET
+from Colin.MetScanning.analyzers.MET import METBuilder
 
 class PFMetAnalyzer( Analyzer ):
 
@@ -39,16 +39,16 @@ class PFMetAnalyzer( Analyzer ):
     def process(self, event):
         self.readCollections( event.input )
         event.met = self.handles['met'].product()[0]
-        print 'PFMET', event.met.pt(), event.met.phi(), event.met.sumEt()
+        # print 'PFMET', event.met.pt(), event.met.phi(), event.met.sumEt()
         #         event.CleanedTrackerAndGlobalMuons = self.handles['CleanedTrackerAndGlobalMuons'].product()
         #     print len(event.CleanedTrackerAndGlobalMuons)
         for name in self.cleaned_pf_names:
             coll = map(PFCandidate, self.handles[name].product())
-            print name, len(coll)
+            # print name, len(coll)
             setattr(event, name, coll)
 
         event.pf = map(PFCandidate, self.handles['pf'].product())
-        print 'pf', len(event.pf)
-        event.rec_pfmet = MET(event.pf, 'particleFlow')
-        print event.rec_pfmet
-
+        # print 'pf', len(event.pf)
+        event.rec_pfmet = METBuilder(event.pf, 'particleFlow')
+        # print event.rec_pfmet
+        print 'diff', event.rec_pfmet.pt() - event.met.pt()
