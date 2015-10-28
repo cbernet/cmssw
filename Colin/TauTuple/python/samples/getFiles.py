6f9fbea3f325d64648546a8de92a621dfb1b28fd
Colin <colin.bernet@cern.ch>
2015-10-28 10:59:16 +0100

    mssm files from lucia

--- Colin/TauTuple/python/samples/getFiles.py
+++ Colin/TauTuple/python/samples/getFiles.py
@@ -3,11 +3,13 @@ from PhysicsTools.HeppyCore.utils.dataset import createDataset
 def getFiles(dataset, cache=True, user='EOS', basedir='/store/cmst3/user/cbern/CMG'):
     ds = createDataset(user, dataset, '.*root', 
                        readcache=cache, basedir=basedir)
-    non_empty_files = []
-    for fname, status in ds.report['Files'].iteritems():
-        if status[1]:
-            non_empty_files.append(str(fname))
-    filenames = non_empty_files
+    filenames = ds.listOfGoodFiles()
+    if user=='CMS':
+        non_empty_files = []
+        for fname, status in ds.report['Files'].iteritems():
+            if status[1]:
+                non_empty_files.append(str(fname))
+        filenames = non_empty_files
     return ['root://eoscms.cern.ch//eos/cms{fname}'.format(fname=fname) 
             for fname in filenames]
 
