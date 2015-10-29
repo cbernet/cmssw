1e78246fa1467f0b8ed8f28b36b7cf967d0156ce
Colin <colin.bernet@cern.ch>
2015-10-29 17:42:51 +0100

    small ztt and qcd samples available

--- Colin/TauTuple/python/samples/lucia.py
+++ Colin/TauTuple/python/samples/lucia.py
@@ -7,7 +7,22 @@ mssm = cfg.Component(
     files = getFiles(
         "/SUSYGluGluToHToTauTau_M-3200_TuneCUETP8M1_13TeV-pythia8/crab_NtupleSUSY", 
         user='EOS', cache=True),
-) 
+    ) 
+
+ztautau = cfg.Component(
+    'ztt',
+    files = getFiles(
+        "/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_NtupleDY/150929_111739", 
+        user='cbern', cache=True),
+    ) 
+
+qcd = cfg.Component(
+    'qcd',
+    files = getFiles(
+        "/QCD_Pt-15to7000_TuneCUETP8M1_Flat_13TeV_pythia8/crab_NtupleQCD/150929_103656", 
+        user='cbern', cache=True),
+    ) 
+
 
 def xrd(fname):
     return '/'.join(['root://xrootd-cms.infn.it//store/user/lperrini',
@@ -38,5 +53,5 @@ mssm_louvain = cfg.Component(
 
 if __name__ == '__main__':
     
-    for f in mssm.files:
+    for f in ztautau.files:
         print f
