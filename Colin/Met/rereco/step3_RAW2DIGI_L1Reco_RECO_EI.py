7128a54144a6ab82d81bd6e62ad0204490626aad
Colin <colin.bernet@cern.ch>
2016-01-20 17:38:52 +0100

    reco in 74X - added correct cmsdriver command

--- Colin/Met/rereco/step3_RAW2DIGI_L1Reco_RECO_EI.py
+++ Colin/Met/rereco/step3_RAW2DIGI_L1Reco_RECO_EI.py
@@ -2,7 +2,7 @@
 # using: 
 # Revision: 1.19 
 # Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
-# with command line options: step3 --datatier GEN-SIM-RECO --conditions auto:run1_mc -s RAW2DIGI,L1Reco,RECO,EI --eventcontent RECOSIM -no_exec -n 100 --filein file:step2.root --fileout file:step3.root
+# with command line options: step3 --datatier RECO --conditions auto:run2_data -s RAW2DIGI,L1Reco,RECO,EI --eventcontent RECO --no_exec -n 100 --filein file:step2.root --fileout file:step3.root --data
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process('RECO')
@@ -12,12 +12,11 @@ process.load('Configuration.StandardSequences.Services_cff')
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
-process.load('SimGeneral.MixingModule.mixNoPU_cfi')
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-process.load('Configuration.StandardSequences.MagneticField_cff')
-process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
 process.load('Configuration.StandardSequences.L1Reco_cff')
-process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.Reconstruction_Data_cff')
 process.load('CommonTools.ParticleFlow.EITopPAG_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
@@ -28,14 +27,7 @@ process.maxEvents = cms.untracked.PSet(
 
 # Input source
 process.source = cms.Source("PoolSource",
-#    fileNames = cms.untracked.vstring('file:step2.root'),
-    fileNames = cms.untracked.vstring(
-        #Myriam
-        'file:/afs/cern.ch/user/m/mschoene/public/pickevents_misRecodMuonRAW.root',
-        # 'file:/afs/cern.ch/user/m/mschoene/public/pickevents_misRecodMuonRAW2.root',
-        #Pawandeep & Owen
-        # 'file:/afs/cern.ch/user/p/pjandir/public/ForEventDisplays/data-bad-events/out-raw-htmht-2015D-1.root'
-        ),
+    fileNames = cms.untracked.vstring('file:/afs/cern.ch/user/m/mschoene/public/pickevents_misRecodMuonRAW.root'),
     secondaryFileNames = cms.untracked.vstring()
 )
 
@@ -52,14 +44,14 @@ process.configurationMetadata = cms.untracked.PSet(
 
 # Output definition
 
-process.RECOSIMoutput = cms.OutputModule("PoolOutputModule",
+process.RECOoutput = cms.OutputModule("PoolOutputModule",
     dataset = cms.untracked.PSet(
-        dataTier = cms.untracked.string('GEN-SIM-RECO'),
+        dataTier = cms.untracked.string('RECO'),
         filterName = cms.untracked.string('')
     ),
     eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
     fileName = cms.untracked.string('file:step3.root'),
-    outputCommands = process.RECOSIMEventContent.outputCommands,
+    outputCommands = process.RECOEventContent.outputCommands,
     splitLevel = cms.untracked.int32(0)
 )
 
@@ -67,8 +59,7 @@ process.RECOSIMoutput = cms.OutputModule("PoolOutputModule",
 
 # Other statements
 from Configuration.AlCa.GlobalTag import GlobalTag
-# process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run1_mc', '')
-process.GlobalTag.globaltag = '75X_dataRun2_v6'
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data', '')
 
 # Path and EndPath definitions
 process.raw2digi_step = cms.Path(process.RawToDigi)
@@ -76,9 +67,9 @@ process.L1Reco_step = cms.Path(process.L1Reco)
 process.reconstruction_step = cms.Path(process.reconstruction)
 process.eventinterpretaion_step = cms.Path(process.EIsequence)
 process.endjob_step = cms.EndPath(process.endOfProcess)
-process.RECOSIMoutput_step = cms.EndPath(process.RECOSIMoutput)
+process.RECOoutput_step = cms.EndPath(process.RECOoutput)
 
 # Schedule definition
-process.schedule = cms.Schedule(process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.eventinterpretaion_step,process.endjob_step,process.RECOSIMoutput_step)
+process.schedule = cms.Schedule(process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.eventinterpretaion_step,process.endjob_step,process.RECOoutput_step)
 
 
