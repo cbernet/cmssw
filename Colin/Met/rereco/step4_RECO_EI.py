2647a549adcd3f27acfc9cd87d4e0b07bae8db4b
Colin <colin.bernet@cern.ch>
2016-01-20 18:23:22 +0100

    rereco in 74 working

--- Colin/Met/rereco/step4_RECO_EI.py
+++ Colin/Met/rereco/step4_RECO_EI.py
@@ -2,7 +2,7 @@
 # using: 
 # Revision: 1.19 
 # Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
-# with command line options: step4 --datatier AODSIM --process reRECO --conditions auto:run1_mc --filtername RECOfromRECO -s RECO,EI --eventcontent AODSIM -no_exec -n 100 --filein file:step3.root --fileout file:step4.root
+# with command line options: step4 --datatier AOD --process reRECO --conditions auto:run2_data --filtername RECOfromRECO -s RECO,EI --eventcontent AOD --no_exec -n 100 --filein file:step3.root --fileout file:step4.root --data
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process('reRECO')
@@ -12,13 +12,12 @@ process.load('Configuration.StandardSequences.Services_cff')
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
-process.load('SimGeneral.MixingModule.mixNoPU_cfi')
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-process.load('Configuration.StandardSequences.MagneticField_cff')
-process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration.StandardSequences.Reconstruction_Data_cff')
 process.load('CommonTools.ParticleFlow.EITopPAG_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(100)
@@ -43,32 +42,31 @@ process.configurationMetadata = cms.untracked.PSet(
 
 # Output definition
 
-process.AODSIMoutput = cms.OutputModule("PoolOutputModule",
+process.AODoutput = cms.OutputModule("PoolOutputModule",
     compressionAlgorithm = cms.untracked.string('LZMA'),
     compressionLevel = cms.untracked.int32(4),
     dataset = cms.untracked.PSet(
-        dataTier = cms.untracked.string('AODSIM'),
+        dataTier = cms.untracked.string('AOD'),
         filterName = cms.untracked.string('RECOfromRECO')
     ),
     eventAutoFlushCompressedSize = cms.untracked.int32(15728640),
     fileName = cms.untracked.string('file:step4.root'),
-    outputCommands = process.AODSIMEventContent.outputCommands
+    outputCommands = process.AODEventContent.outputCommands
 )
 
 # Additional output definition
 
 # Other statements
-from Configuration.AlCa.GlobalTag import GlobalTag
-# process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run1_mc', '')
-process.GlobalTag.globaltag = '75X_dataRun2_v6'
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data', '')
 
 # Path and EndPath definitions
 process.reconstruction_step = cms.Path(process.reconstruction_fromRECO)
 process.eventinterpretaion_step = cms.Path(process.EIsequence)
 process.endjob_step = cms.EndPath(process.endOfProcess)
-process.AODSIMoutput_step = cms.EndPath(process.AODSIMoutput)
+process.AODoutput_step = cms.EndPath(process.AODoutput)
 
 # Schedule definition
-process.schedule = cms.Schedule(process.reconstruction_step,process.eventinterpretaion_step,process.endjob_step,process.AODSIMoutput_step)
+process.schedule = cms.Schedule(process.reconstruction_step,process.eventinterpretaion_step,process.endjob_step,process.AODoutput_step)
 
 
